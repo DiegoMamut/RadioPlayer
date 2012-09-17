f6726e57d9ded3984399d5ffd3828311cf1f9ca6
Karthik Paithankar <karthik.1@example.com>
2012-09-17 19:10:19 +0200

    resize images
    add npr stream
    
    add npr logo

--- src/com/biophysics/radioplayer/AACPlayerActivity.java
+++ src/com/biophysics/radioplayer/AACPlayerActivity.java
@@ -468,7 +468,8 @@ public class AACPlayerActivity extends Activity implements
 	}
 
 	private String getUrlOne() throws Exception {
-		String ret = "mms://wmlive-nonacl.bbc.net.uk/wms/bbc_ami/radio1/radio1_bb_live_int_ep1_sl0";
+		//String ret = "mms://wmlive-nonacl.bbc.net.uk/wms/bbc_ami/radio1/radio1_bb_live_int_ep1_sl0";
+		String ret = "mms://a1671.l2063252432.c20632.g.lm.akamaistream.net/D/1671/20632/v0001/reflector:52432";
 		
 		return ret;
 	}
